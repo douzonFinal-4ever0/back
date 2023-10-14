80a3fe2cf5314861aeee8bbd2f170b8d4c8fb8e0
Juwon <jjw1578@gmail.com>
2023-10-14 23:03:37 +0900

    10/14 jjw 공지사항 작성 로직 완성

--- src/main/java/com/kosa/resq/service/mr/MrAdminService.java
+++ src/main/java/com/kosa/resq/service/mr/MrAdminService.java
@@ -3,11 +3,13 @@ package com.kosa.resq.service.mr;
 import com.kosa.resq.domain.dto.mr.MrDTO;
 import com.kosa.resq.domain.dto.mr.MrKeyWordDTO;
 import com.kosa.resq.domain.dto.mr.MrRezDTO;
+import com.kosa.resq.domain.dto.mr.NoticeDTO;
 
 import java.util.List;
 
 public interface MrAdminService {
     public List<MrDTO> mrList();
-    public List<MrDTO> test();
-    public List<MrRezDTO> test2();
+    public List<MrRezDTO> mrRez();
+    public List<NoticeDTO> noticeList();
+    public void addNotice(NoticeDTO notice);
 }
