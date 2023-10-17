b6b3de3300862c24e16be645347532db6b85aab7
h2jin <tkdwls1229@naver.com>
2023-10-17 09:29:04 +0900

    10/16 lhj 사원 전체 조회 로직 생성

--- src/main/java/com/kosa/resq/mapper/car/CarAdminMapper.java
+++ src/main/java/com/kosa/resq/mapper/car/CarAdminMapper.java
@@ -2,6 +2,7 @@ package com.kosa.resq.mapper.car;
 
 import com.kosa.resq.domain.vo.car.CarRequestVO;
 import com.kosa.resq.domain.vo.car.CarResponseVO;
+import com.kosa.resq.domain.vo.common.MemResponseVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Date;
@@ -11,5 +12,6 @@ import java.util.List;
 public interface CarAdminMapper {
     public void carSave(CarRequestVO carRequestVO);
     public List<CarResponseVO> carGetAll();
+    public List<MemResponseVO> memGetAll();
 
 }
--- src/main/java/com/kosa/resq/service/car/CarAdminService.java
+++ src/main/java/com/kosa/resq/service/car/CarAdminService.java
@@ -1,7 +1,9 @@
 package com.kosa.resq.service.car;
 
+import com.kosa.resq.domain.dto.common.MemDTO;
 import com.kosa.resq.domain.vo.car.CarRequestVO;
 import com.kosa.resq.domain.vo.car.CarResponseVO;
+import com.kosa.resq.domain.vo.common.MemResponseVO;
 
 import java.util.List;
 
@@ -9,4 +11,5 @@ public interface CarAdminService {
 
     public void carSave(CarRequestVO carVO);
     public List<CarResponseVO> carGetAll();
+    public List<MemDTO> memGetAll();
 }
