265448ab6b1d7fd45f39d45b3a350cc33c26ce86
Juwon <jjw1578@gmail.com>
2023-10-17 17:58:40 +0900

    10/17 jjw 회의실 등록 수정 완료, 명명법에 따라 수정

--- src/main/java/com/kosa/resq/domain/dto/mr/MrRezDTO.java
+++ src/main/java/com/kosa/resq/domain/dto/mr/MrRezDTO.java
@@ -1,12 +1,13 @@
 package com.kosa.resq.domain.dto.mr;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Getter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MrRezDTO {
