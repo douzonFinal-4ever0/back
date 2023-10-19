d29e97cd7ac49f65dac34549752c4debdd7203b2
h2jin <tkdwls1229@naver.com>
2023-10-19 17:20:52 +0900

    10/19 lhj 차량 리스트 정보 받는 VO 이름 수정

--- src/main/java/com/kosa/resq/domain/vo/car/CarListResponseVO.java
+++ src/main/java/com/kosa/resq/domain/vo/car/CarListResponseVO.java
@@ -9,10 +9,10 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CarResponseVO {
+public class CarListResponseVO {
     private String type;
-    private String car_code;
     private String car_name;
+    private String car_code;
     private Date created_at;
     private int accum_mileage;
     private String memo;
