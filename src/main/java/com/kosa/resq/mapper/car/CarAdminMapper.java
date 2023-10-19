af1520e148e5581bd62aeefbe1ebc203c7b4d6b2
h2jin <tkdwls1229@naver.com>
2023-10-19 17:21:23 +0900

    10/19 lhj 차량 상세 정보 로직 구현

--- src/main/java/com/kosa/resq/mapper/car/CarAdminMapper.java
+++ src/main/java/com/kosa/resq/mapper/car/CarAdminMapper.java
@@ -1,13 +1,9 @@
 package com.kosa.resq.mapper.car;
 
-import com.kosa.resq.domain.vo.car.CarDetailRequestVO;
-import com.kosa.resq.domain.vo.car.CarRequestVO;
-import com.kosa.resq.domain.vo.car.CarResponseVO;
-import com.kosa.resq.domain.vo.car.CarUserRequestVO;
+import com.kosa.resq.domain.vo.car.*;
 import com.kosa.resq.domain.vo.common.MemResponseVO;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Date;
 import java.util.List;
 
 @Mapper
@@ -15,8 +11,9 @@ public interface CarAdminMapper {
     public void carSave(CarRequestVO carRequestVO);
     void carDetailSave(CarDetailRequestVO carDetailRequestVO);
     void carUserSave(CarUserRequestVO carUserRequestVO);
-    CarResponseVO carGetOne(String car_code);
-    public List<CarResponseVO> carGetAll();
+    CarListResponseVO carListGetOne(String car_code);
+    public List<CarListResponseVO> carGetAll();
     public List<MemResponseVO> memGetAll();
+    public CarDetailResponseVO carGetOne(String car_code);
 
 }
--- src/main/java/com/kosa/resq/service/car/CarAdminService.java
+++ src/main/java/com/kosa/resq/service/car/CarAdminService.java
@@ -1,18 +1,15 @@
 package com.kosa.resq.service.car;
 
 import com.kosa.resq.domain.dto.common.MemDTO;
-import com.kosa.resq.domain.vo.car.CarDetailRequestVO;
-import com.kosa.resq.domain.vo.car.CarRequestVO;
-import com.kosa.resq.domain.vo.car.CarResponseVO;
-import com.kosa.resq.domain.vo.car.CarUserRequestVO;
-import com.kosa.resq.domain.vo.common.MemResponseVO;
+import com.kosa.resq.domain.vo.car.*;
 
 import java.util.List;
 
 public interface CarAdminService {
 
     public void carSave(CarRequestVO carVO, CarDetailRequestVO carDetailRequestVO, CarUserRequestVO carUserRequestVO);
-    public List<CarResponseVO> carGetAll();
-    CarResponseVO carGetOne(String car_code);
+    public List<CarListResponseVO> carGetAll();
+    CarListResponseVO carListGetOne(String car_code);
     public List<MemDTO> memGetAll();
+    public CarDetailResponseVO carGetOne(String car_code);
 }
