c5f42462de1a949cabdb8316089c6895dc11f376
esther <wjswls109@naver.com>
2023-10-30 18:06:30 +0900

    10/30 jest 등록, 변경, 삭제일자 추가

--- src/main/java/com/kosa/resq/domain/vo/mr/BmGroupMemVO.java
+++ src/main/java/com/kosa/resq/domain/vo/mr/BmGroupMemVO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +13,8 @@ public class BmGroupMemVO {
     private String bm_code; // 즐겨찾기 번호
     private String bm_group_code; // 즐겨찾기 그룹 번호 (*개인이면 null)
     private String mem_code; // 사번 (지정자)
+    private Date created_at; // 등록일자
+    private Date updated_at; // 변경일자
+    private Date deleted_at; // 삭제일자
 }
+
--- src/main/java/com/kosa/resq/domain/vo/mr/BmGroupVO.java
+++ src/main/java/com/kosa/resq/domain/vo/mr/BmGroupVO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +13,7 @@ public class BmGroupVO {
     private String bm_group_code; // 즐겨찾기 그룹 번호
     private String mem_code; // 사번 (소유자)
     private String bm_group_name; // 그룹명
+    private Date created_at; // 등록일자
+    private Date updated_at; // 변경일자
+    private Date deleted_at; // 삭제일자
 }
